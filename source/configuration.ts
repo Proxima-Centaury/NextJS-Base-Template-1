041a9a64c4ed3c552af7f9ebe0ac87125c93e8cb
ProximaCentaury <rainbow-life@outlook.fr>
2024-08-31 14:38:40 +0200

    Adding locales and themes fallback to configuration file

--- source/configuration.ts
+++ source/configuration.ts
@@ -3,15 +3,21 @@ import type TConfiguration from ">_/types/TConfiguration";
 // Configuration -------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
 const configuration: TConfiguration = {
     locales: [ `en`, `fr` ],
+    localesFallback: `en`,
     projectName: `NextJS Base Template I`,
     themes: [ `dark`, `light` ],
+    themesFallback: `light`,
     version: `1.0`
 };
 // Property ------------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
 const locales: TConfiguration[`locales`] = configuration.locales;
 // Property ------------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
+const localesFallback: TConfiguration[`localesFallback`] = configuration.localesFallback;
+// Property ------------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
 const themes: TConfiguration[`themes`] = configuration.themes;
+// Property ------------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
+const themesFallback: TConfiguration[`themesFallback`] = configuration.themesFallback;
 // Default Export ------------------------------------------------------------------------------------------------------------------------------------------------------ [ EXPORTS ]
 export default configuration;
 // Exports ------------------------------------------------------------------------------------------------------------------------------------------------------------- [ EXPORTS ]
-export { locales, themes };
+export { locales, localesFallback, themes, themesFallback };
