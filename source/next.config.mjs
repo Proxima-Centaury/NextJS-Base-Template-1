5a1d0fa5e7024a80c5c02c5ae2fa97bca12906bb
ProximaCentaury <rainbow-life@outlook.fr>
2024-10-24 21:37:15 +0200

    Enabling Webpack cache

--- source/next.config.mjs
+++ source/next.config.mjs
@@ -1,6 +1,24 @@
 // Dependencies -------------------------------------------------------------------------------------------------------------------------------------------------------- [ IMPORTS ]
+import { fileURLToPath } from "url";
 import createNextIntlPlugin from "next-intl/plugin";
 // Configuration -------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
 const withNextInternationalization = createNextIntlPlugin(`./i18n.ts`);
+// Variable ------------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
+const __filename = fileURLToPath(import.meta.url);
+// Configuration -------------------------------------------------------------------------------------------------------------------------------------------------- [ DECLARATIONS ]
+/** @type { import("next").NextConfig } */
+const nextConfiguration = {
+    webpack: (config, { isServer }) => {
+        if(!isServer) {
+            config.cache = {
+                type: `filesystem`,
+                buildDependencies: {
+                    config: [ __filename ]
+                }
+            }
+        };
+        return config;
+    }
+};
 // Default Export ------------------------------------------------------------------------------------------------------------------------------------------------------ [ EXPORTS ]
-export default withNextInternationalization({});
+export default withNextInternationalization(nextConfiguration);
